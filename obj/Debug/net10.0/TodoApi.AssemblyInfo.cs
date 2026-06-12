4cf2aa9999adefb7803735cc2f0a43cf705a32ea
stenkula <ninni.stenkula@hotmail.com>
2026-06-12 12:31:05 +0200

    Opprett en enkel postgres DB

--- obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8147ba06da6b5633f70f6d646f1ff735da9aec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98c030e5b4db943854f403ae382e2d848335ed5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
